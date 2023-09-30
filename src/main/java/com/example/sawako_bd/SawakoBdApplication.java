ecce84d5a970b19d4f93bfce1d20b558b318fc91
aser161 <aser161@yandex.ru>
2023-09-30 16:01:42 +0300

    Create repository

---
+++ src/main/java/com/example/sawako_bd/SawakoBdApplication.java
@@ -0,0 +1,13 @@
+package com.example.sawako_bd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SawakoBdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SawakoBdApplication.class, args);
+	}
+
+}
